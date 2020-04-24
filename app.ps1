157c6da3e1577e52813e30ebff2de3df410d753e
Harish <harish.karthic@gmail.com>
2020-04-24 18:48:11 +0100

    Moved all the mandatory settings to settings.json file

--- app.ps1
+++ app.ps1
@@ -5,6 +5,15 @@ This script defines the app layout by importing and processing necessary module.
 .DESCRIPTION
 This script is intented to import the module which contains news app scripts
 and runs it by passing necessary parameters.
+
+.NOTES
+Author             Version		 Date			Notes
+----------------------------------------------------------------------
+harish.karthic     1.0	        22/04/2020		Initial script
+harish.karthic     1.1	        24/04/2020		Minor tweak
+harish.karthic     1.3	        24/04/2020		Removed hard coded values and moved
+                                                mandatory settings to settings.json
+                                                file.
 #>
 
 #region module refresh
@@ -12,37 +21,35 @@ and runs it by passing necessary parameters.
 Import-Module .\Module
 
 # run functions
-$UK = @{
-    ApiKey = (Import-Clixml .\Key.clixml).GetNetworkCredential().Password
-    Url = 'http://newsapi.org/v2/top-headlines?country=gb'
-    ExportPath = ".\index.html"
-    Country = "UK"
-    ReferenceName = "INDIA"
-    ReferenceLink = "india.html"
-    LogPath = ".\Logs"
-    Verbose = $true
-}
 
-$INDIA = @{
-    ApiKey = (Import-Clixml .\Key.clixml).GetNetworkCredential().Password
-    Url = 'http://newsapi.org/v2/top-headlines?country=in'
-    ExportPath = ".\india.html"
-    Country = "INDIA"
-    ReferenceName = "UK"
-    ReferenceLink = "index.html"
-    LogPath = ".\Logs"
-    Verbose = $true
+$settings = Get-Content -Path ".\settings.json" -Raw | ConvertFrom-Json
+
+$countries = @("UK", "INDIA")
+
+foreach ($country in $countries) {
+
+    $params = @{
+        ApiKey = (Import-Clixml $settings.parameters.apiKey).GetNetworkCredential().Password
+        Url = $settings.parameters.countries.$($country).url
+        ExportPath = $settings.parameters.countries.$($country).fileName
+        Country = $settings.parameters.countries.$($country).name
+        ReferenceName = $settings.parameters.countries.$($country).referenceName
+        ReferenceLink = $settings.parameters.countries.$($country).referenceLink
+        LogPath = $settings.parameters.reportPaths.logPath
+        Verbose = $true
+    }
+
+    Read-News @params
+
 }
 
 $CovidData = @{
-    Url = "https://thevirustracker.com/free-api?global=stats"
-    CountryUrl = "https://thevirustracker.com/free-api?countryTimeline=GB"
-    FilePath = ".\Reports"
-    ExportPath = ".\"
-    LogPath = ".\Logs"
+    Url = $settings.parameters.covid19.url
+    CountryUrl = $settings.parameters.covid19.countryUrl
+    FilePath = $settings.parameters.covid19.filePath
+    ExportPath = $settings.parameters.reportPaths.exportPath
+    LogPath = $settings.parameters.reportPaths.logPath
     Verbose = $true
 }
 
-Read-News @UK
-Read-News @INDIA
 Get-CovidData @CovidData
